3b3d9d485918407c8db62d065d8d17af5397edb5
Akolo <c25samuel.akolo@afacademy.af.edu>
2024-09-24 10:37:11 -0600

    Added code to analyze and detect nearby Wifi networks

--- src/signal_processing/signal_detector.py
+++ src/signal_processing/signal_detector.py
@@ -1,9 +1,14 @@
 # src/signal_processing/signal_detector.py
 
 import time
-import numpy as np
+import os
+import sys
+import pandas as pd
+from threading import Thread
 from rtlsdr import RtlSdr
-from scapy.all import *
+import scapy.all as scapy
+from scapy.layers.dot11 import Dot11Beacon, Dot11Elt, Dot11
+
 
 
 class SignalDetector:
@@ -77,5 +82,34 @@ class SignalDetector:
 
 #WIFI SIGNAL ANALYSIS / SNIFFING
 
-def packet_capture(pkt):
-    print('Source IP: '+ pkt[IP].src + 'Destination IP: ' + pkt[IP].dst)
+if sys.version_info < (3, 0):
+    sys.stderr.write("\nYou need python 3.0 or later to run this script\n")
+    sys.stderr.write("Please update and make sure you use the command python3 wifi_scanner.py <interface>\n\n")
+    sys.exit(0)
+
+networks = pd.DataFrame(columns=["BSSID", "SSID", "dBm_Signal", "Channel", "Encryption"])
+networks.set_index("BSSID", inplace=True)
+
+
+def process_packet(packet):
+    """this function get executed whenever a packet is sniffed to process it"""
+    if packet.haslayer(Dot11Beacon):
+        bssid = packet[Dot11].addr2  # get the MAC addresses of the networks
+        ssid = str(packet[Dot11Elt].info, encoding="UTF-8")  # get the name of the networks
+        ssid = "<Hidden>" if ssid == "" else str(packet[Dot11Elt].info, encoding="UTF-8")  # check if the network
+        # doesn't broadcast its name (ternary operator)
+        dbm_signal = packet.dBm_AntSignal  # get the signal strength of the networks
+
+        stats = packet[Dot11Beacon].network_stats()  # extract network stats <channel, rate, encryption, etc..>
+        channel = stats.get("channel")  # get the channel number of the networks
+        enc = stats.get("crypto")  # get the encryption of the networks
+
+        networks.loc[bssid] = (ssid, dbm_signal, channel, enc)  # putting all together
+
+
+def print_networks():
+    """this function clear terminal every 0.7s and print wi-fi networks again"""
+    while True:
+        os.system("clear")
+        print(networks)
+        time.sleep(0.7)
